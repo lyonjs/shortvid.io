045649ae4d3e419402a575c0866e07bde49eb960
Antoine Caron <antoine.caron@bedrockstreaming.com>
2022-09-11 00:23:45 +0200

    feat(chapo): enhance design of chapo

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,10 +13,32 @@ export default ({Component, pageProps}) => {
 				<header>
 					<LyonJSLogo style={{height: '40px', display: 'block'}} />
 					<h1>LyonJS - Social video preview generator</h1>
+					<a
+						className="github"
+						href="https://github.com/lyonjs/social-video-generator"
+					>
+						Repository
+					</a>
 				</header>
 				<p>
-					This application allow you to preview videos directly in the browser
-					before generating them with github action workflows.
+					This application is developed and maintained by the{' '}
+					<a href="https://github.com/lyonjs/social-video-generator/graphs/contributors">
+						volunteer organizers
+					</a>{' '}
+					of the <a href="http://lyonjs.org/">Lyon JS Meetup</a>. It aims to
+					facilitate the generation of video for{' '}
+					<a href="https://twitter.com/lyonjs">
+						the social networks of the association
+					</a>
+					.
+				</p>
+				<p>
+					Videos can be generated through Github workflow or directly locally if
+					you have installed the project.
+				</p>
+				<p>
+					This project uses <a href="https://nextjs.org/">NextJS</a> and{' '}
+					<a href="https://www.remotion.dev/">Remotion</a> libraries.
 				</p>
 			</article>
 

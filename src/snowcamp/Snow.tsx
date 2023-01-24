c189e85ad2945ea4755a70f5f34cec9aaa9996f5
Antoine Caron <Slashgear@users.noreply.github.com>
2023-01-24 18:57:21 +0100

    feat: adding snowcamp generator (#97)
    * add design proposal for snowcamp
    
    * implement new template
    
    * add logo
    
    * feat: adding snowcamp generator in app (#96)
    
    Co-authored-by: Alves Mickaël <alves.mckl@gmail.com>

---
+++ src/snowcamp/Snow.tsx
@@ -0,0 +1,25 @@
+import {Lottie} from '@remotion/lottie';
+import {useLottie} from '../components/hooks/useLottie';
+import {interpolate, useCurrentFrame} from 'remotion';
+
+export const Snow = () => {
+	const frame = useCurrentFrame();
+	const snow = useLottie('lf20_kZx06J');
+
+	const opacity = interpolate(frame, [0, 20], [0, 1]);
+
+	if (!snow) {
+		return null;
+	}
+
+	return (
+		<Lottie
+			style={{
+				width: '100%',
+				height: '100%',
+				opacity,
+			}}
+			animationData={snow}
+		/>
+	);
+};
---
+++ src/snowcamp/Snowcamp.tsx
@@ -0,0 +1,48 @@
+import {AbsoluteFill, Sequence} from 'remotion';
+import {Title} from './Title';
+import {Details} from './Details';
+import {Speakers} from './Speakers';
+import {Background} from './Background';
+import React from 'react';
+import {Snow} from './Snow';
+import {Logo} from './Logo';
+
+export interface Speaker {
+	picture: string;
+	name: string;
+}
+
+export interface TouraineTechProps {
+	title: string;
+	speakers: Speaker[];
+	date: string;
+	time: string;
+	location: string;
+}
+
+export const Snowcamp: React.FC<TouraineTechProps> = ({
+	title,
+	speakers,
+	date,
+	time,
+	location,
+}) => {
+	return (
+		<AbsoluteFill style={{backgroundColor: 'white', overflow: 'hidden'}}>
+			<Sequence>
+				<Background />
+			</Sequence>
+			<Sequence from={20}>
+				<Snow />
+			</Sequence>
+			<Sequence from={50}>
+				<Logo />
+				{speakers && <Speakers speakers={speakers} />}
+				<Title title={title} />
+			</Sequence>
+			<Sequence from={70}>
+				<Details date={date} time={time} location={location} />
+			</Sequence>
+		</AbsoluteFill>
+	);
+};

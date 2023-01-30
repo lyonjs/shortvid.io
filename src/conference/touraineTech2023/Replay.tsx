47682fba61807dddc4704926789325c2feb0fe6d
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-01-30 21:38:53 +0100

    feat: add video replay template for touraine tech

---
+++ src/conference/touraineTech2023/Replay.tsx
@@ -0,0 +1,46 @@
+import {Sequence} from 'remotion';
+import {Logo} from './Logo';
+import {RightTriangle} from './RightTriangle';
+import {LeftTriangle} from './LeftTriangle';
+import {Speakers} from './Speakers';
+import {Title} from './Title';
+import {Details} from './Details';
+import {AbsoluteFill} from 'remotion';
+import {Speaker} from './TouraineTech2023';
+import {useLottie} from '../../components/hooks/useLottie';
+import {Lottie} from '@remotion/lottie';
+import {Type} from './Type';
+import {BigSpeakers} from './BigSpeakers';
+
+interface ReplayType {
+	title: string;
+	type: string;
+	speakers: Speaker[];
+}
+
+export const Replay: React.FC<ReplayType> = ({title, speakers, type}) => {
+	return (
+		<AbsoluteFill style={{backgroundColor: 'white', overflow: 'hidden'}}>
+			<Logo />
+			<RightTriangle />
+			<LeftTriangle />
+			<BigSpeakers speakers={speakers} dropTop={250} />
+			<Sequence from={60}>
+				<Title
+					title={title}
+					style={{
+						fontSize: '48px',
+						margin: '0 auto',
+						bottom: '200px',
+						padding: '0 60px',
+						left: 0,
+						right: 0,
+					}}
+				/>
+			</Sequence>
+			<Sequence from={120} name="Details">
+				<Type type={type} />
+			</Sequence>
+		</AbsoluteFill>
+	);
+};

f2950802099032e32d7db52b84425bb917db5182
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-02-19 14:34:10 +0100

    feat(footer): add vercel banner in footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import '../styles/globals.css';
 import {Header} from '../src/components/site/Header';
@@ -75,7 +76,20 @@ export default ({Component, pageProps}) => {
 			</article>
 			<NavBar />
 			<Component {...pageProps} />
-			<footer className="mt-6 text-right">Made with 💛 and JS</footer>
+			<footer className="flex justify-between items-center my-8">
+				<a
+					className="block"
+					href="https://vercel.com?utm_source=lyonjs&utm_campaign=oss"
+				>
+					<Image
+						alt="Vercel"
+						width="200"
+						height="100"
+						src="https://images.ctfassets.net/e5382hct74si/78Olo8EZRdUlcDUFQvnzG7/fa4cdb6dc04c40fceac194134788a0e2/1618983297-powered-by-vercel.svg"
+					/>
+				</a>
+				<span className="block">Made with 💛 and JS</span>
+			</footer>
 		</main>
 	);
 };

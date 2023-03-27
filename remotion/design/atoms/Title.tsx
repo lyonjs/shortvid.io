38bf964b08545b0929ee140d260dbb8057b10ede
emeric <emericboy73@gmail.com>
2023-03-27 17:09:46 +0200

    Fix: lint errors from atom/title

--- remotion/design/atoms/Title.tsx
+++ remotion/design/atoms/Title.tsx
@@ -3,25 +3,25 @@ export const Title: React.FC<{
 	style?: CSSProperties;
 	children: React.ReactNode;
 }> = ({style, children}) => (
-		<h1
-			style={{
-				fontFamily: 'Helvetica',
-				fontSize: 50,
-				fontWeight: 'bold',
-				margin: 0,
-				width: '100%',
-				height: "max-content",
-				display: '-webkit-box',
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				"-webkit-box-orient": 'vertical',
-				"-webkit-line-clamp": '2',
-				textOverflow: 'ellipsis',
-				overflow:'hidden',
-				padding: '0 20px',
-				...style,
-			}}
-		>
-			{children}
-		</h1>
+	<h1
+		style={{
+			fontFamily: 'Helvetica',
+			fontSize: 50,
+			fontWeight: 'bold',
+			margin: 0,
+			width: '100%',
+			height: 'max-content',
+			display: '-webkit-box',
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			'-webkit-box-orient': 'vertical',
+			'-webkit-line-clamp': '2',
+			textOverflow: 'ellipsis',
+			overflow: 'hidden',
+			padding: '0 20px',
+			...style,
+		}}
+	>
+		{children}
+	</h1>
 );

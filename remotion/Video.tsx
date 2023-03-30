9d032eff1b995a5cafc7d6c0c960aa4565e61b18
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-03-30 16:48:47 +0200

    chore(remotion): reorganise remotion preview to fit project organisation (#227)

--- remotion/Video.tsx
+++ remotion/Video.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import {Folder} from 'remotion';
 import {ShowcasesComposition} from './compositions/showcases/Showcases.composition';
 import {TemplatesComposition} from './compositions/templates/Templates.composition';
+import {AtomsComposition} from './design/atoms/Atoms.composition';
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<TemplatesComposition />
 			<ShowcasesComposition />
+			<Folder name="Design">
+				<AtomsComposition />
+			</Folder>
 		</>
 	);
 };
--- remotion/compositions/templates/Templates.composition.tsx
+++ remotion/compositions/templates/Templates.composition.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Folder} from 'remotion';
 
-import {AtomsComposition} from '../../design/atoms/Atoms.composition';
 import {SponsorsComposition} from './sponsor/Sponsors.composition';
 import {TalksComposition} from './talk/Talks.composition';
 import {MeetupComposition} from './meetup/Meetup.composition';
@@ -10,7 +9,6 @@ import {EventsComposition} from './event/Events.composition';
 export const TemplatesComposition: React.FC = () => {
 	return (
 		<Folder name="Templates">
-			<AtomsComposition />
 			<SponsorsComposition />
 			<TalksComposition />
 			<MeetupComposition />

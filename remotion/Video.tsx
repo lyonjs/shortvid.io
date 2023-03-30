0faf09c34ac9b435de13794580cdf7d7745d5922
emeric <emericboy73@gmail.com>
2023-03-30 16:03:54 +0200

    Reorganise remotion preview to fit project organisation

--- remotion/Video.tsx
+++ remotion/Video.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import {Folder} from 'remotion';
 import {ShowcasesComposition} from './compositions/showcases/Showcases.composition';
 import {TemplatesComposition} from './compositions/templates/Templates.composition';
+import {AtomsComposition} from './design/atoms/Atoms.composition';
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
-			<TemplatesComposition />
-			<ShowcasesComposition />
+			<Folder name="Compositions">
+				<TemplatesComposition />
+				<ShowcasesComposition />
+			</Folder>
+			<Folder name="Design">
+				<AtomsComposition />
+			</Folder>
 		</>
 	);
 };

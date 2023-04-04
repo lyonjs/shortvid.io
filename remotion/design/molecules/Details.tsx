94da2f767b52f374b2a516dbfe2cfbf715497a26
emeric <emericboy73@gmail.com>
2023-04-04 16:37:14 +0200

    Created molecule Details.tsx

---
+++ remotion/design/molecules/Details.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {IconWithCaption} from '../atoms/IconWithCaption';
+import {AbsoluteFill} from 'remotion';
+
+export const Details: React.FC<{
+	date?: string;
+	dateStyle?: React.CSSProperties;
+	time?: string;
+	timeStyle?: React.CSSProperties;
+	location?: string;
+	locationStyle?: React.CSSProperties;
+	style?: React.CSSProperties;
+}> = ({date, dateStyle, time, timeStyle, location, locationStyle, style}) => {
+	return (
+		<AbsoluteFill style={{background: 'black'}}>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexWrap: 'wrap',
+					columnGap: '7rem',
+					position: 'absolute',
+					bottom: '3rem',
+					width: '100%',
+					fontWeight: 700,
+					color: 'white',
+					...style,
+				}}
+			>
+				{date && (
+					<IconWithCaption
+						lottieAsset="lf20_ak90tqhe"
+						caption={date}
+						style={dateStyle}
+					/>
+				)}
+				{time && (
+					<IconWithCaption
+						lottieAsset="lf20_nv5aXa"
+						caption={time}
+						iconStyle={{width: 80}}
+						style={{gap: '2rem', ...timeStyle}}
+					/>
+				)}
+				{location && (
+					<IconWithCaption
+						lottieAsset="lf20_PgZU3O"
+						caption={location}
+						iconStyle={{width: 90, ...locationStyle}}
+					/>
+				)}
+			</div>
+		</AbsoluteFill>
+	);
+};
--- remotion/design/molecules/Molecules.composition.tsx
+++ remotion/design/molecules/Molecules.composition.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Composition, Folder} from 'remotion';
+import {Composition, Folder, staticFile} from 'remotion';
 import {AvatarWithCaption} from './AvatarWithCaption';
 import {IconWithCaption} from './IconWithCaption';
+import {Details} from './Details';
 
 export const MoleculesComposition: React.FC = () => {
 	return (
@@ -31,6 +32,19 @@ export const MoleculesComposition: React.FC = () => {
 						color: 'black',
 						padding: 20,
 					},
+				}}
+			/>
+			<Composition
+				component={Details}
+				width={1200}
+				height={1200}
+				id="Details"
+				fps={30}
+				durationInFrames={150}
+				defaultProps={{
+					date: '19 Décembre 2023',
+					time: '17h30',
+					location: 'Salle 1',
 				}}
 			/>
 		</Folder>

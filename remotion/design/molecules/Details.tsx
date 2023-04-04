05d81cce96a28788f588eae0ac294e1711ae7275
emeric <emericboy73@gmail.com>
2023-04-04 16:37:49 +0200

    Remove absolue fill

--- remotion/design/molecules/Details.tsx
+++ remotion/design/molecules/Details.tsx
@@ -12,45 +12,43 @@ export const Details: React.FC<{
 	style?: React.CSSProperties;
 }> = ({date, dateStyle, time, timeStyle, location, locationStyle, style}) => {
 	return (
-		<AbsoluteFill style={{background: 'black'}}>
-			<div
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					flexWrap: 'wrap',
-					columnGap: '7rem',
-					position: 'absolute',
-					bottom: '3rem',
-					width: '100%',
-					fontWeight: 700,
-					color: 'white',
-					...style,
-				}}
-			>
-				{date && (
-					<IconWithCaption
-						lottieAsset="lf20_ak90tqhe"
-						caption={date}
-						style={dateStyle}
-					/>
-				)}
-				{time && (
-					<IconWithCaption
-						lottieAsset="lf20_nv5aXa"
-						caption={time}
-						iconStyle={{width: 80}}
-						style={{gap: '2rem', ...timeStyle}}
-					/>
-				)}
-				{location && (
-					<IconWithCaption
-						lottieAsset="lf20_PgZU3O"
-						caption={location}
-						iconStyle={{width: 90, ...locationStyle}}
-					/>
-				)}
-			</div>
-		</AbsoluteFill>
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexWrap: 'wrap',
+				columnGap: '7rem',
+				position: 'absolute',
+				bottom: '3rem',
+				width: '100%',
+				fontWeight: 700,
+				color: 'white',
+				...style,
+			}}
+		>
+			{date && (
+				<IconWithCaption
+					lottieAsset="lf20_ak90tqhe"
+					caption={date}
+					style={dateStyle}
+				/>
+			)}
+			{time && (
+				<IconWithCaption
+					lottieAsset="lf20_nv5aXa"
+					caption={time}
+					iconStyle={{width: 80}}
+					style={{gap: '2rem', ...timeStyle}}
+				/>
+			)}
+			{location && (
+				<IconWithCaption
+					lottieAsset="lf20_PgZU3O"
+					caption={location}
+					iconStyle={{width: 90, ...locationStyle}}
+				/>
+			)}
+		</div>
 	);
 };

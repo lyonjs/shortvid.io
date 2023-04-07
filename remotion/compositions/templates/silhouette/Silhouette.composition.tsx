5e84b0c72a2f9404e1e48118a074391df8df4bd8
Antoine Caron <antoine.caron@bedrockstreaming.com>
2023-04-07 21:58:19 +0200

    feat(templates): add a new generic template

---
+++ remotion/compositions/templates/silhouette/Silhouette.composition.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Composition, Folder} from 'remotion';
+import {Silhouette} from './Silhouette';
+
+export const SilhouetteComposition: React.FC = () => {
+	return (
+		<Folder name="Silhouette">
+			<Composition
+				component={Silhouette}
+				width={1280}
+				height={720}
+				id="Silhouette"
+				fps={30}
+				durationInFrames={120}
+				defaultProps={{
+					backgroundImg:
+						'https://images.unsplash.com/photo-1550537687-c91072c4792d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+					title: 'Example of title for the template 🚀',
+					silhouetteUrl:
+						'https://user-images.githubusercontent.com/6263857/230662773-4d7a534a-e01c-4ba8-9c3b-fa95586adf52.png',
+					side: 'right',
+				}}
+			/>
+		</Folder>
+	);
+};
---
+++ remotion/compositions/templates/silhouette/SilhouetteTitle.tsx
@@ -0,0 +1,38 @@
+import {Title} from '../../../design/atoms/Title';
+import React from 'react';
+import {
+	Easing,
+	interpolate,
+	spring,
+	useCurrentFrame,
+	useVideoConfig,
+} from 'remotion';
+
+export const SilhouetteTitle: React.FC<{title: string}> = ({title}) => {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+
+	const drop = spring({
+		frame,
+		fps,
+		from: 30,
+		to: 50,
+	});
+
+	const opacity = interpolate(frame, [0, 30], [0, 1], {
+		extrapolateRight: 'clamp',
+		easing: Easing.out(Easing.ease),
+	});
+
+	return (
+		<Title
+			style={{
+				position: 'absolute',
+				top: `${drop}%`,
+				opacity,
+			}}
+		>
+			{title}
+		</Title>
+	);
+};

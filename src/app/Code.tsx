587966020b454813129395443a70108f14b95173
CruuzAzul <alves.mckl@gmail.com>
2023-04-10 10:49:03 +0200

    fix prettier

--- src/app/Code.tsx
+++ src/app/Code.tsx
@@ -36,8 +36,9 @@ export const Code: React.FC<{
 					className=" cursor-pointer rounded-lg block py-3 px-5 break-all text-lg bg-stone-900 hover:bg-stone-700"
 					onClick={onClickHandler}
 				>
-					pnpm remotion render remotion/index.tsx {composition} out/{composition}.mp4
-					--props=&apos;{JSON.stringify(params).replace(/'/g, "'\\''")}&apos;
+					pnpm remotion render remotion/index.tsx {composition} out/
+					{composition}.mp4 --props=&apos;
+					{JSON.stringify(params).replace(/'/g, "'\\''")}&apos;
 				</code>
 				<div className="notif absolute py-2 px-3 bg-amber-300 text-neutral-900 rounded-lg opacity-0 right-0 top-0 pointer-events-none">
 					Command copied in clipboard

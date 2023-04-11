cb107dab3687bdfce7949d73553224070a179652
emeric <emericboy73@gmail.com>
2023-04-11 15:45:56 +0200

    Updated touraine tech details

--- remotion/compositions/showcases/touraineTech2023/Details.tsx
+++ remotion/compositions/showcases/touraineTech2023/Details.tsx
@@ -1,6 +1,7 @@
 import {Lottie} from '@remotion/lottie';
 import {useLottie} from '../../../hooks/useLottie';
 import {Sequence, spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {TalkDetails} from '../../../design/molecules/TalkDetails';
 
 export const Details: React.FC<{
 	date: string;
@@ -9,110 +10,37 @@ export const Details: React.FC<{
 }> = ({date, time, location}) => {
 	const frame = useCurrentFrame();
 	const {fps} = useVideoConfig();
-	const illustrationDate = useLottie('lf20_ak90tqhe');
-	const illustrationHour = useLottie('lf20_nv5aXa');
-	const illustrationLocation = useLottie('lf20_PgZU3O');
 
-	const drop = spring({frame, from: -20, to: 0, fps, durationInFrames: 30});
-	const opacity = spring({frame, from: 0, to: 1, fps, durationInFrames: 30});
-
-	if (!illustrationHour || !illustrationDate || !illustrationLocation) {
-		return null;
-	}
+	const drop = spring({
+		frame: frame - 40,
+		from: -20,
+		to: 0,
+		fps,
+		durationInFrames: 30,
+	});
+	const opacity = spring({
+		frame: frame - 40,
+		from: 0,
+		to: 1,
+		fps,
+		durationInFrames: 30,
+	});
 
 	return (
 		<Sequence from={40} name="Details">
-			<div
+			<TalkDetails
+				date={date}
+				time={time}
+				location={location}
 				style={{
 					fontFamily: 'Noto Sans,sans-serif',
-					fontWeight: 700,
 					fontSize: '32px',
-					color: 'white',
-					position: 'absolute',
-					bottom: 0,
-					display: 'flex',
-					width: '100%',
-					justifyContent: 'space-around',
-					alignItems: 'center',
 				}}
-			>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						gap: '1rem',
-					}}
-				>
-					<Lottie
-						style={{
-							width: '130px',
-							filter: 'none',
-						}}
-						playbackRate={1.5}
-						animationData={illustrationDate}
-					/>
-					<span
-						style={{
-							position: 'relative',
-							bottom: drop,
-							opacity,
-						}}
-					>
-						{date}
-					</span>
-				</div>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						gap: '2rem',
-					}}
-				>
-					<Lottie
-						style={{
-							width: '70px',
-							filter: 'none',
-						}}
-						animationData={illustrationHour}
-					/>
-					<span
-						style={{
-							position: 'relative',
-							bottom: drop,
-							opacity,
-						}}
-					>
-						{time}
-					</span>
-				</div>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-						gap: '1rem',
-					}}
-				>
-					<Lottie
-						style={{
-							width: '80px',
-							filter: 'none',
-						}}
-						animationData={illustrationLocation}
-					/>
-					<span
-						style={{
-							position: 'relative',
-							bottom: drop,
-							opacity,
-						}}
-					>
-						{location}
-					</span>
-				</div>
-			</div>
+				iconStyle={{
+					opacity,
+					bottom: drop,
+				}}
+			/>
 		</Sequence>
 	);
 };
--- remotion/compositions/showcases/touraineTech2023/Type.tsx
+++ remotion/compositions/showcases/touraineTech2023/Type.tsx
@@ -1,52 +1,23 @@
-import {Lottie} from '@remotion/lottie';
-import {useLottie} from '../../../hooks/useLottie';
 import {spring, useCurrentFrame, useVideoConfig} from 'remotion';
+import {TalkDetails} from '../../../design/molecules/TalkDetails';
+import React from 'react';
 
 export const Type: React.FC<{type: string}> = ({type}) => {
 	const frame = useCurrentFrame();
 	const {fps} = useVideoConfig();
-	const illustrationHour = useLottie('lf20_nv5aXa');
 
 	const drop = spring({frame, from: -20, to: 0, fps, durationInFrames: 30});
 	const opacity = spring({frame, from: 0, to: 1, fps, durationInFrames: 30});
 
-	if (!illustrationHour) {
-		return null;
-	}
-
 	return (
-		<div
+		<TalkDetails
+			time={type}
 			style={{
-				position: 'absolute',
-				bottom: 30,
-				left: 0,
-				right: 0,
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				gap: '2rem',
-				fontFamily: 'Noto Sans,sans-serif',
-				fontWeight: 700,
 				fontSize: '32px',
-				color: 'white',
+				gridTemplateColumns: '1fr',
+				fontFamily: 'Noto Sans,sans-serif',
 			}}
-		>
-			<Lottie
-				style={{
-					width: '70px',
-					filter: 'none',
-				}}
-				animationData={illustrationHour}
-			/>
-			<span
-				style={{
-					position: 'relative',
-					bottom: drop,
-					opacity,
-				}}
-			>
-				{type}
-			</span>
-		</div>
+			iconStyle={{opacity, bottom: drop}}
+		/>
 	);
 };

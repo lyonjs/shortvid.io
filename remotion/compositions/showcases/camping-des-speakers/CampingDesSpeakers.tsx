8503be60ad4f6370c012f6275ae558888b7b215a
emeric <emericboy73@gmail.com>
2023-05-11 17:17:57 +0200

    Add default content for talks

--- remotion/compositions/showcases/camping-des-speakers/CampingDesSpeakers.tsx
+++ remotion/compositions/showcases/camping-des-speakers/CampingDesSpeakers.tsx
@@ -1,12 +1,39 @@
-import {AbsoluteFill, staticFile} from 'remotion';
-import {RemotionRiveCanvas} from '@remotion/rive';
+import {AbsoluteFill, Sequence} from 'remotion';
+import {TalkTitle} from './TalkTitle';
+import {Details} from './Details';
+import {Speakers} from './Speakers';
+import React from 'react';
 
-export const CampingDesSpeakers = () => {
+export interface Speaker {
+	picture: string;
+	name: string;
+	company: string;
+}
+
+export interface Props {
+	title: string;
+	speakers: Speaker[];
+	date: string;
+	time: string;
+	location: string;
+}
+
+export const CampingDesSpeakers: React.FC<Props> = ({
+	title,
+	speakers,
+	date,
+	time,
+	location,
+}) => {
 	return (
-		<AbsoluteFill style={{background: 'red'}}>
-			<RemotionRiveCanvas
-				src={staticFile('images/conferences/campingDesSpeakers/beach.riv')}
-			/>
+		<AbsoluteFill style={{backgroundColor: 'black', overflow: 'hidden'}}>
+			<Sequence from={50}>
+				{speakers && <Speakers speakers={speakers} />}
+				<TalkTitle title={title} />
+			</Sequence>
+			<Sequence from={70}>
+				<Details date={date} time={time} location={location} />
+			</Sequence>
 		</AbsoluteFill>
 	);
 };

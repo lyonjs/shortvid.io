c9e933aab7a9c2303553e918ce6955b40e6e327b
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-05-16 22:10:35 +0200

    feat(app): add showcase camping des speakers (#309)
    Co-authored-by: CruuzAzul <alves.mckl@gmail.com>

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -29,7 +29,7 @@ export const NavBar: React.FC = () => {
 						<span>🎉 Event</span>
 					</li>
 				</ActiveLink>
-				<ActiveLink href="/showcases/conferences/Mixit">
+				<ActiveLink href="/showcases/conferences/CampingDesSpeakers">
 					<li className="text-black text-center mt-2 md:mt-0 md:ml-5 py-2 px-4 bg-white rounded-lg cursor-pointer font-bold shadow-yellow-300 hover:scale-105">
 						<span>🫱🏼‍🫲🏽 Conference</span>
 					</li>

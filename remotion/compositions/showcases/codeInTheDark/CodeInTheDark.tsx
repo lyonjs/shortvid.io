258c63ad106d3ab813ba301b086ab196e4b80095
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-05-19 16:35:39 +0200

    feat(showcase): create code in the dark video (#310)
    Co-authored-by: CruuzAzul <alves.mckl@gmail.com>

---
+++ remotion/compositions/showcases/codeInTheDark/CodeInTheDark.tsx
@@ -0,0 +1,42 @@
+import {AbsoluteFill, Audio, Sequence, staticFile} from 'remotion';
+import {Combo} from './Combo';
+import {Spotlight} from './Spotlight';
+import {Words} from './Words';
+import Details from './Details';
+import {Background} from './Background';
+import {Organisateurs} from './Organisateurs';
+import {loadLocalFont} from '../../../../src/app/utils/loadFont';
+
+loadLocalFont('PressStart2P', 'font/PressStart2P.ttf', 'truetype');
+
+export const CodeInTheDark = () => {
+	return (
+		<AbsoluteFill>
+			<Sequence name="KeyboardAudio" durationInFrames={255}>
+				<Audio
+					src={staticFile('sounds/keyboard.mp3')}
+					startFrom={130}
+					volume={0.4}
+				/>
+			</Sequence>
+			<Sequence name="Background" from={40}>
+				<Background />
+			</Sequence>
+			<Sequence name="Details" from={40}>
+				<Details date="20 juin" />
+			</Sequence>
+			<Sequence name="Organisateurs" from={40}>
+				<Organisateurs />
+			</Sequence>
+			<Sequence name="Sportlight" durationInFrames={130}>
+				<Spotlight />
+			</Sequence>
+			<Sequence name="Combo">
+				<Combo />
+			</Sequence>
+			<Sequence name="PopingWords">
+				<Words />
+			</Sequence>
+		</AbsoluteFill>
+	);
+};
---
+++ remotion/compositions/showcases/codeInTheDark/Details.tsx
@@ -0,0 +1,84 @@
+import {useEffect, useState} from 'react';
+import {interpolate, useCurrentFrame} from 'remotion';
+import {loadFont} from '@remotion/google-fonts/AnonymousPro';
+
+const {fontFamily} = loadFont();
+
+const Details: React.FC<{date: string}> = ({date}) => {
+	const frame = useCurrentFrame();
+	const [text, setText] = useState<string[]>([]);
+	const tagStyle = {margin: 2, color: '#08fe79'};
+	const detailsText = `Mettez vos talents en CSS à l'épreuve lors de la première édition Lyonnaise de Code In The Dark ! Rejoignez-nous le ${date} pour une soirée de compétition et de fun !`;
+	const detailsStyleText = [
+		'infos {',
+		'organisateurs: LyonJS;',
+		'lieu: Zenika;',
+		'date: 20 juin 2023;',
+		'heure: 19h00;',
+	];
+	const styleTag = ['<', 's', 't', 'y', 'l', 'e', '>'];
+	const styleClosingTag = ['<', '/', 's', 't', 'y', 'l', 'e', '>'];
+
+	const animateLetters = (textArray: string[], delay?: number) => {
+		return textArray.map((letter, index) => {
+			const managedDelay = delay ? delay + index : index;
+			const letterAppear = interpolate(frame - managedDelay, [0, 1], [0, 1], {
+				extrapolateRight: 'clamp',
+				extrapolateLeft: 'clamp',
+			});
+			return (
+				<span key={index} style={{opacity: letterAppear}}>
+					{letter}
+				</span>
+			);
+		});
+	};
+
+	useEffect(() => {
+		const splitedText = detailsText.split('');
+		setText(splitedText);
+	}, [detailsText]);
+
+	return (
+		<div
+			style={{
+				fontFamily,
+				fontSize: '2rem',
+				position: 'relative',
+				padding: 20,
+				maxWidth: '70%',
+			}}
+		>
+			<p style={tagStyle}>&lt;div&gt;</p>
+			<p style={{margin: 0, paddingLeft: 40, lineHeight: 1.5, color: 'white'}}>
+				{animateLetters(text)}
+			</p>
+			<p style={tagStyle}>&lt;/div&gt;</p>
+			<br />
+
+			<p style={tagStyle}>{animateLetters(styleTag, 135)}</p>
+			<div style={{margin: 0, paddingLeft: 40, lineHeight: 1.2}}>
+				{detailsStyleText.map((line, index) => {
+					const splitedLine = line.split('');
+
+					return (
+						<p
+							key={index}
+							style={{
+								color: 'white',
+								paddingLeft: index !== 0 ? 30 : 0,
+								margin: 10,
+							}}
+						>
+							{animateLetters(splitedLine, 140 + index * 12)}
+						</p>
+					);
+				})}
+				<p style={{color: 'white', margin: 10}}>{animateLetters(['}'], 195)}</p>
+			</div>
+			<p style={tagStyle}>{animateLetters(styleClosingTag, 196)}</p>
+		</div>
+	);
+};
+
+export default Details;

df56ce661df765f0f346cfa12e863eecf59fa7c1
emeric <emericboy73@gmail.com>
2023-05-25 11:08:48 +0200

    review changes: renamed img

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => {
 	return (
 		<header className="flex items-center my-9">
 			<Image
-				src="/Monogram-white.svg"
+				src="/monogram-white.svg"
 				alt="Shortvid.io Logo"
 				width={30}
 				height={30}

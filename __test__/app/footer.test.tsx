07e3361426d53b98dc4228d378f3f3128e9f1ef4
emeric <emericboy73@gmail.com>
2023-05-31 11:51:04 +0200

    review changes: add describe tot test

--- __test__/app/footer.test.tsx
+++ __test__/app/footer.test.tsx
@@ -2,29 +2,31 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import {Footer} from '../../src/app/Footer';
 
-it('Loads and display the component "Footer"', () => {
-	render(<Footer />);
+describe('Loads and display the component "Footer"', () => {
+	it('render the component', () => {
+		render(<Footer />);
 
-	const logo = screen.getByRole('img', {
-		name: 'Shortvid.io logo',
+		const logo = screen.getByRole('img', {
+			name: 'Shortvid.io logo',
+		});
+
+		const vercelImg = screen.getByRole('img', {
+			name: 'Vercel',
+		});
+		const vercelLink = screen.getByRole('link', {
+			name: 'Vercel',
+		});
+
+		const footer = screen.getByRole('contentinfo');
+
+		expect(logo).toBeVisible();
+
+		expect(vercelImg).toBeVisible();
+		expect(vercelLink).toBeVisible();
+
+		expect(footer).toBeVisible();
+		expect(footer.textContent).toEqual(
+			'© Copyright 2023 LyonJs - Made with 💛 and JS'
+		);
 	});
-
-	const vercelImg = screen.getByRole('img', {
-		name: 'Vercel',
-	});
-	const vercelLink = screen.getByRole('link', {
-		name: 'Vercel',
-	});
-
-	const footer = screen.getByRole('contentinfo');
-
-	expect(logo).toBeVisible();
-
-	expect(vercelImg).toBeVisible();
-	expect(vercelLink).toBeVisible();
-
-	expect(footer).toBeVisible();
-	expect(footer.textContent).toEqual(
-		'© Copyright 2023 LyonJs - Made with 💛 and JS'
-	);
 });

689261e0731bebbb4c129944779cc9088e976978
emeric <emericboy73@gmail.com>
2023-06-05 15:53:29 +0200

    created buttons to switch theme

--- src/app/SwitchThemeButton.tsx
+++ src/app/SwitchThemeButton.tsx
@@ -1,21 +1,62 @@
 'use client';
 
 import {useTheme} from 'next-themes';
-import {useThemeName} from './hooks/useThemeName';
-import styles from '../../styles/app/common/button.module.css';
+import {Icon} from '@iconify/react';
+import {useState} from 'react';
+import styles from '../../styles/app/inputs/button.module.css';
+
+type selectedThemeTypes = 'dark' | 'light' | 'system';
 
 export const SwitchThemeButton = () => {
-	const {setTheme} = useTheme();
-	const theme = useThemeName();
-
-	const checkTheme = theme === 'dark' ? 'light' : 'dark';
+	const {setTheme, systemTheme} = useTheme();
+	const [selectedTheme, setSelectedTheme] = useState<string>(
+		localStorage.getItem('theme') || 'system'
+	);
+	const handleSelectTheme = (
+		newTheme: string,
+		clickedButton: selectedThemeTypes
+	) => {
+		setTheme(newTheme);
+		setSelectedTheme(clickedButton);
+	};
 
 	return (
-		<button
-			className={styles.switchThemeButton}
-			onClick={() => setTheme(checkTheme)}
-		>
-			Switch to {checkTheme} mode
-		</button>
+		<div className={styles.switchThemeGroup} role="radiogroup">
+			<button
+				aria-checked={selectedTheme === 'dark'}
+				aria-label="Dark"
+				className=""
+				role="radio"
+				type="button"
+				value="dark"
+				onClick={(e) => handleSelectTheme(e.currentTarget.value, 'dark')}
+			>
+				<SwitchIcon iconifyId="ph:moon" />
+			</button>
+			<button
+				aria-checked={selectedTheme === 'light'}
+				aria-label="Light"
+				role="radio"
+				type="button"
+				value="light"
+				onClick={(e) => handleSelectTheme(e.currentTarget.value, 'light')}
+			>
+				<SwitchIcon iconifyId="ph:sun" />
+			</button>
+			<button
+				aria-checked={selectedTheme === 'system'}
+				aria-label="System"
+				role="radio"
+				type="button"
+				value={systemTheme}
+				onClick={(e) => handleSelectTheme(e.currentTarget.value, 'system')}
+			>
+				<SwitchIcon iconifyId="gg:screen" />
+			</button>
+		</div>
 	);
+};
+
+const SwitchIcon: React.FC<{iconifyId: string}> = ({iconifyId}) => {
+	return <Icon icon={iconifyId} width={20} height={20} />;
 };

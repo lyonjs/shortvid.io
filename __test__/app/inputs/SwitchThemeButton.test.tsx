e8640538f221b489707a23472dbd122d41bea3a0
emeric <emericboy73@gmail.com>
2023-06-12 09:30:35 +0200

    updated
    test with userEvent

--- __test__/app/inputs/SwitchThemeButton.test.tsx
+++ __test__/app/inputs/SwitchThemeButton.test.tsx
@@ -1,10 +1,7 @@
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import {fireEvent, render, screen} from '@testing-library/react';
-import {Footer} from '../../../src/app/Footer';
-import {useSelectedTheme} from '../../../src/app/hooks/useSelectedTheme';
-
-jest.mock('../../../src/app/hooks/useSelectedTheme.ts');
+import {render, screen} from '@testing-library/react';
+import {SwitchThemeButtons} from '../../../src/app/SwitchThemeButtons';
 
 describe('<SwitchThemeButton />', () => {
 	const getButtons = () => ({
@@ -13,10 +10,11 @@ describe('<SwitchThemeButton />', () => {
 		darkButton: screen.getByLabelText('dark'),
 	});
 
-	const testThemeSwitch = (targetButton: HTMLElement) => {
+	const testThemeSwitch = async (targetButton: HTMLElement) => {
+		const user = userEvent.setup();
 		const {systemButton, lightButton, darkButton} = getButtons();
 
-		fireEvent.click(targetButton);
+		await user.click(targetButton);
 
 		expect(lightButton.getAttribute('checked')).toEqual(
 			targetButton === lightButton ? 'true' : 'false'
@@ -30,12 +28,7 @@ describe('<SwitchThemeButton />', () => {
 	};
 
 	beforeEach(() => {
-		(useSelectedTheme as jest.MockedFunction<any>).mockReturnValue({
-			themeName: 'dark',
-			selectedTheme: 'system',
-			setSelectedTheme: jest.fn(),
-		});
-		render(<Footer />);
+		render(<SwitchThemeButtons />);
 		const {systemButton, lightButton, darkButton} = getButtons();
 
 		expect(systemButton).toBeChecked();
@@ -60,24 +53,22 @@ describe('<SwitchThemeButton />', () => {
 	});
 
 	it('should switch to the light theme', () => {
-		const user = userEvent.setup();
 		const {lightButton} = getButtons();
 
 		expect(lightButton).not.toBeChecked();
-
-		user.click(lightButton);
-		expect(lightButton).toBeChecked();
-		// testThemeSwitch(lightButton);
+		testThemeSwitch(lightButton);
 	});
-	//
-	// it('should switch to the dark theme', () => {
-	// 	const {darkButton} = getButtons();
-	// 	testThemeSwitch(darkButton);
-	// });
-	//
-	// it('should switch to the system theme', () => {
-	// 	const {lightButton, systemButton} = getButtons();
-	// 	testThemeSwitch(lightButton);
-	// 	testThemeSwitch(systemButton);
-	// });
+
+	it('should switch to the dark theme', () => {
+		const {darkButton} = getButtons();
+
+		expect(darkButton).not.toBeChecked();
+		testThemeSwitch(darkButton);
+	});
+
+	it('should switch to the system theme', () => {
+		const {lightButton, systemButton} = getButtons();
+		testThemeSwitch(lightButton);
+		testThemeSwitch(systemButton);
+	});
 });

d93459dcf61e2db901879dbbd94cd86ecf16375c
emeric <emericboy73@gmail.com>
2023-07-06 09:34:54 +0200

    rebase update arrow and header-footer refacto

--- src/app/components/sidebar/Header.tsx
+++ src/app/components/sidebar/Header.tsx
@@ -1,12 +1,18 @@
 import styles from '../../../../styles/app/components/sidebar/sidebar.module.css';
 import {Logo} from '../../Logo';
+import {MuseoModerno} from 'next/font/google';
+
+const wormarkFont = MuseoModerno({
+	weight: '900',
+	subsets: ['latin'],
+});
 
 export const Header: React.FC<{folded: boolean}> = ({folded}) => {
 	return (
 		<header>
 			<a href="/" className={styles.logoLink}>
 				<Logo />
-				{!folded && <h1>Shortvid.io</h1>}
+				{!folded && <h1 className={wormarkFont.className}>Shortvid.io</h1>}
 			</a>
 		</header>
 	);
--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import {Nav} from './Nav';
 import React, {useState} from 'react';
 import {Header} from './Header';
 import {Footer} from './Footer';
+import {Icon} from '@iconify/react';
 
 export const Sidebar = () => {
 	const [folded, setFolded] = useState<boolean>(false);
@@ -18,7 +19,9 @@ export const Sidebar = () => {
 				type="button"
 				className={styles.foldButton}
 				onClick={() => setFolded(!folded)}
-			/>
+			>
+				<Icon icon="iconamoon:arrow-left-2" />
+			</button>
 		</section>
 	);
 };

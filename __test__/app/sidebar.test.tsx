913d313e6c01b616eb89bb387636868516f1c4a4
emeric <emericboy73@gmail.com>
2023-07-06 16:20:59 +0200

    completed sidebar test

--- __test__/app/sidebar.test.tsx
+++ __test__/app/sidebar.test.tsx
@@ -1,9 +1,25 @@
 import '@testing-library/jest-dom';
-import {render, screen, waitFor} from '@testing-library/react';
+import {sideBarNavConfigMock} from '../mocks/sideBarConfig.mock';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {Sidebar} from '../../src/app/components/sidebar/Sidebar';
 
 jest.mock('next/navigation', () => ({
 	usePathname: jest.fn().mockReturnValue('/'),
+}));
+
+jest.mock('../../src/data/config/sideBarConfig', () => ({
+	sideBarNavConfig: sideBarNavConfigMock,
+}));
+
+jest.mock('../../src/app/components/sidebar/CompositionThumbnail', () => ({
+	CompositionThumbnail: ({compositionName}: {compositionName: string}) => {
+		return (
+			<div>
+				<h3>{compositionName}</h3>
+			</div>
+		);
+	},
 }));
 
 describe('<Sidebar />', () => {
@@ -12,34 +28,71 @@ describe('<Sidebar />', () => {
 	});
 
 	it('should render the component', () => {
+		const sidebar = screen.getByTestId('sidebar');
 		const logo = screen.getByTitle('Shortvid.io Logo').parentNode;
-
-		const contributingLink = screen.getByText('Contributing');
+		const contributingLink = screen.getByText('Contributing').parentNode;
 		const themeButtonsGroup = screen.getByRole('radiogroup');
-
 		const nav = screen.getByRole('navigation');
 
-		const topLevelLink = screen.getByRole('listitem', {
-			name: 'home',
-		});
-
+		const topLevelLink = screen.getByText('topLevel');
 		const video = screen.getByRole('heading', {
-			name: 'Event',
+			name: 'Video',
 		});
-
 		const categorisedVideo = screen.getByRole('heading', {
-			name: 'Talk',
+			name: 'CategorisedVideo',
 		});
 
-		waitFor(() => {
-			expect(logo).toBeVisible();
-			expect(contributingLink).toBeVisible();
-			expect(themeButtonsGroup).toBeVisible();
-			expect(nav).toBeVisible();
+		expect(sidebar).toBeVisible();
+		expect(logo).toBeVisible();
+		expect(contributingLink).toBeVisible();
+		expect(themeButtonsGroup).toBeVisible();
 
-			expect(topLevelLink).toBeVisible();
-			expect(video).toBeVisible();
-			expect(categorisedVideo).toBeVisible();
+		expect(nav).toBeVisible();
+
+		expect(topLevelLink).toBeVisible();
+		expect(video).not.toBeVisible();
+		expect(categorisedVideo).not.toBeVisible();
+	});
+
+	it('should display videos list', async () => {
+		const user = userEvent.setup();
+		const topLevelLink = screen.getByText('topLevel').parentNode as Element;
+		const firstLevelVideo = screen.getByRole('heading', {
+			name: 'Video',
 		});
+		const categoryButton = screen.getByText('category');
+
+		expect(topLevelLink).toBeVisible();
+		expect(firstLevelVideo).not.toBeVisible();
+		expect(categoryButton).not.toBeVisible();
+
+		await user.click(topLevelLink);
+
+		expect(firstLevelVideo).toBeVisible();
+		expect(categoryButton).toBeVisible();
+	});
+
+	it('should fold', async () => {
+		const user = userEvent.setup();
+
+		const foldButton = screen.getByRole('button', {
+			name: 'foldButton',
+		});
+		const logoWordmark = screen.getByRole('heading', {
+			name: 'Shortvid.io',
+		});
+		const topLevelText = screen.getByText('topLevel');
+		const contributingText = screen.getByText('Contributing');
+
+		expect(foldButton).toBeVisible();
+		expect(logoWordmark).toBeVisible();
+		expect(topLevelText).toBeVisible();
+		expect(contributingText).toBeVisible();
+
+		await user.click(foldButton);
+
+		expect(logoWordmark).not.toBeVisible();
+		expect(topLevelText).not.toBeVisible();
+		expect(contributingText).not.toBeVisible();
 	});
 });
---
+++ __test__/mocks/sideBarConfig.mock.ts
@@ -0,0 +1,23 @@
+export const sideBarNavConfigMock = {
+	topLevel: {
+		iconifyId: 'ic:round-slow-motion-video',
+		route: '/toplevel/',
+		items: [
+			{
+				categoryName: 'category',
+				items: [
+					{
+						compositionName: 'CategorisedVideo',
+						compositionId: 'CategorisedVideo',
+						compositionLink: 'categorisedVideo',
+					},
+				],
+			},
+			{
+				compositionName: 'Video',
+				compositionId: 'Video',
+				compositionLink: 'video',
+			},
+		],
+	},
+};

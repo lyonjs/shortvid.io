026f8229ef0b9685c8eaece1d8951eda2e6c571f
emeric <emericboy73@gmail.com>
2023-07-07 11:54:13 +0200

    review changes: remove useless css var + used module.css on rootLayout

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -6,12 +6,13 @@ import {FontProvider} from '../context/FontContext';
 import {Footer} from './Footer';
 import {CustomThemeProvider} from '../context/CustomThemeProvider';
 import {Sidebar} from './components/sidebar/Sidebar';
+import styles from '../../styles/app/layout/main.module.css';
 
 export default function RootLayout({children}: {children: ReactNode}) {
 	return (
 		<CustomThemeProvider>
 			<main className="mx-auto p-4">
-				<div className="sidebarLayout">
+				<div className={styles.sidebarLayout}>
 					<Sidebar />
 					<section>
 						<FontProvider>{children}</FontProvider>

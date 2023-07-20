3508d521b065fcadf20dde40dac2128e07a9b7e7
emeric <emericboy73@gmail.com>
2023-07-20 17:24:36 +0200

    updated eslint config to work fo tests

--- __test__/app/footer.test.tsx
+++ __test__/app/footer.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
+
 import {Footer} from '../../src/app/Footer';
 
 describe('<Footer />', () => {
--- __test__/app/inputs/SwitchThemeButton.test.tsx
+++ __test__/app/inputs/SwitchThemeButton.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import {SwitchThemeButtons} from '../../../src/app/SwitchThemeButtons';
 
 describe('<SwitchThemeButton />', () => {

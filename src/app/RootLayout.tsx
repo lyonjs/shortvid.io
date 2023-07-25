ee6503aad93732cae92bdf4c82dbc91aff0e544b
Alves Mickaël <alves.mckl@gmail.com>
2023-07-25 09:00:40 +0200

    feat(app): only display footer on homepage (#446)

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -1,36 +1,31 @@
 import {ReactNode} from 'react';
 import {Analytics} from '@vercel/analytics/react';
 
-import {CustomThemeProvider} from '../context/CustomThemeProvider';
 import {FontProvider} from '../context/FontContext';
 import {SidebarProvider} from '../context/SidebarContext';
 
 import {Sidebar} from './components/sidebar/navigation/Sidebar';
 import {SidebarMobile} from './components/sidebar/navigation/SidebarMobile';
 import {ResizeWrapper} from './components/sidebar/ResizeWrapper';
-import {Footer} from './Footer';
 
 import '../../styles/globals.css';
 import styles from '../../styles/app/layout/main.module.css';
 
 export default function RootLayout({children}: {children: ReactNode}) {
 	return (
-		<CustomThemeProvider>
-			<main className="mx-auto p-4">
-				<div className={styles.sidebarLayout}>
-					<SidebarProvider>
-						<ResizeWrapper>
-							<Sidebar />
-						</ResizeWrapper>
-						<SidebarMobile />
-					</SidebarProvider>
-					<section>
-						<FontProvider>{children}</FontProvider>
-						<Footer />
-					</section>
-				</div>
-				<Analytics />
-			</main>
-		</CustomThemeProvider>
+		<main className="mx-auto p-4">
+			<div className={styles.sidebarLayout}>
+				<SidebarProvider>
+					<ResizeWrapper>
+						<Sidebar />
+					</ResizeWrapper>
+					<SidebarMobile />
+				</SidebarProvider>
+				<section>
+					<FontProvider>{children}</FontProvider>
+				</section>
+			</div>
+			<Analytics />
+		</main>
 	);
 }

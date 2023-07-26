caad67fe11522e4b354114a2793e3d2d59190317
Emeric Boyadjian <72607059+Boyadjie@users.noreply.github.com>
2023-07-26 15:34:07 +0200

    feat(app): poc refacto pages with sidebar right (#444)

--- src/app/forms/Form.tsx
+++ src/app/forms/Form.tsx
@@ -1,7 +1,63 @@
-import React, {ReactNode} from 'react';
+import React, {FormEvent, ReactNode} from 'react';
+
+import {CopyUrlButton} from '../CopyUrlButton';
+import {QueryParams} from '../utils/formatUrlWithQuery';
+
+import {InputProps} from './input';
+import {InputDateProps} from './inputDate';
+import {SelectInputProps} from './selectInput';
 
 import styles from '../../../styles/app/common/form.module.css';
 
-export const Form: React.FC<{children: ReactNode}> = ({children}) => {
-	return <form className={styles.videoForm}>{children}</form>;
+type FormInputTypes =
+	| React.FC<InputProps>
+	| React.FC<InputDateProps>
+	| React.FC<SelectInputProps>;
+
+export type formConfigProps = {
+	[key: string]: {
+		state: string | Date | undefined;
+		setState: (event: FormEvent<HTMLInputElement | HTMLSelectElement>) => void;
+		label: string;
+		component: FormInputTypes;
+		placeholder?: string;
+		options?: string[];
+	};
+};
+
+export const Form: React.FC<{
+	formConfig?: formConfigProps;
+	encodedParams?: QueryParams;
+	children?: ReactNode;
+}> = ({formConfig, encodedParams, children}) => {
+	return (
+		<>
+			<form className={styles.videoForm}>
+				{formConfig && (
+					<>
+						{Object.keys(formConfig).map((value) => {
+							const inputParams = formConfig[value];
+							const InputComponent = inputParams.component;
+							const stateValue = inputParams.state as
+								| (string & Date)
+								| undefined;
+
+							return (
+								<InputComponent
+									key={inputParams.label}
+									value={stateValue}
+									setValue={inputParams.setState}
+									label={inputParams.label}
+									placeholder={inputParams.placeholder}
+									options={inputParams.options}
+								/>
+							);
+						})}
+					</>
+				)}
+				{children}
+			</form>
+			{encodedParams && <CopyUrlButton urlParameters={encodedParams} />}
+		</>
+	);
 };

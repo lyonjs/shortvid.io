1a5ba9125fc7e66f6a546f1bcf869bb440cf0a7f
emeric <emericboy73@gmail.com>
2023-07-27 15:16:32 +0200

    review changes: created full screen button for layers

--- app/templates/layers/layers/page.tsx
+++ app/templates/layers/layers/page.tsx
@@ -1,32 +1,39 @@
 'use client';
 
-import {useCallback, useState} from 'react';
 import {Player} from '@remotion/player';
 
 import {ResizeWrapper} from '../../../../src/app/components/sidebar/ResizeWrapper';
 import {Sidebar} from '../../../../src/app/components/sidebar/Sidebar';
 import {ColorInput} from '../../../../src/app/forms/colorInput';
 import {Form, FormConfigProps} from '../../../../src/app/forms/Form';
+import {FullScreenButtons} from '../../../../src/app/forms/FullScreenButtons';
 import {Input} from '../../../../src/app/forms/input';
 import {SelectInput} from '../../../../src/app/forms/selectInput';
 import {useInputChange} from '../../../../src/app/hooks/useInputChange';
 import LayerByMode from '../../../../src/app/LayerByMode';
 import {LayerMode} from '../../../../src/app/types/layerMode.types';
+import {encodeObjectValues} from '../../../../src/app/utils/encodeObjectValues';
 
-import stylesBtn from '../../../../styles/app/common/buttons.module.css';
 import styles from '../../../../styles/app/layout/main.module.css';
 
 export default function LayersPage() {
-	const [copied, setCopied] = useState(false);
-	const [mode, setMode] = useInputChange<LayerMode>('one');
-	const [title, setTitle] = useInputChange<string>('Shortvid.io 🎬');
+	const [mode, setMode] = useInputChange<LayerMode>('one', 'mode');
+	const [title, setTitle] = useInputChange<string>('Shortvid.io 🎬', 'title');
 	const [sponsor, setSponsor] = useInputChange<string>(
 		'https://secure.meetupstatic.com/photos/event/2/8/7/a/600_464230362.jpeg',
+		'sponsor',
 	);
-	const [primaryColor, setPrimaryColor] = useInputChange<string>('#323330');
-	const [secondaryColor, setSecondaryColor] = useInputChange<string>('#efdb4f');
+	const [primaryColor, setPrimaryColor] = useInputChange<string>(
+		'#323330',
+		'primaryColor',
+	);
+	const [secondaryColor, setSecondaryColor] = useInputChange<string>(
+		'#efdb4f',
+		'secondaryColor',
+	);
 	const [decoration, setDecoration] = useInputChange<string>(
 		'https://user-images.githubusercontent.com/60877626/232909816-ca4e61c0-acb2-469b-95dc-beed0cb6b482.png',
+		'decoration',
 	);
 
 	const props = {
@@ -37,18 +44,14 @@ export default function LayersPage() {
 		secondaryColor,
 		decoration,
 	};
+
+	const encodedParams = encodeObjectValues(props);
+
 	const layerUrl = `/screens/layers/${mode}/?title=${title}&sponsor=${sponsor}&primaryColor=${encodeURIComponent(
 		primaryColor,
 	)}&secondaryColor=${encodeURIComponent(
 		secondaryColor,
 	)}&decoration=${decoration}`;
-
-	const onClickHandler = useCallback(() => {
-		const {origin} = window.location;
-		navigator.clipboard.writeText(`${origin}${layerUrl}`);
-		setCopied(true);
-		setTimeout(() => setCopied(false), 2000);
-	}, [layerUrl]);
 
 	const formConfig: FormConfigProps = {
 		mode: {
@@ -109,46 +112,22 @@ export default function LayersPage() {
 				/>
 
 				<div className={styles.formMobile}>
-					<Form formConfig={formConfig}>
-						<button
-							type="button"
-							className={stylesBtn.secondaryButton}
-							onClick={() => {
-								window.open(layerUrl, '_blank');
-							}}
-						>
-							Display Layer 👀
-						</button>
-						<button
-							type="button"
-							className={stylesBtn.primaryButton}
-							onClick={onClickHandler}
-						>
-							{copied ? 'Copied ✅' : 'Copy Layer URL 🔗'}
-						</button>
+					<Form formConfig={formConfig} encodedParams={encodedParams}>
+						<FullScreenButtons
+							compositionName="Layer"
+							fullScreenUrl={layerUrl}
+						/>
 					</Form>
 				</div>
 			</section>
 
 			<ResizeWrapper resizableSide="left">
 				<Sidebar>
-					<Form formConfig={formConfig}>
-						<button
-							type="button"
-							className={stylesBtn.secondaryButton}
-							onClick={() => {
-								window.open(layerUrl, '_blank');
-							}}
-						>
-							Display Layer 👀
-						</button>
-						<button
-							type="button"
-							className={stylesBtn.primaryButton}
-							onClick={onClickHandler}
-						>
-							{copied ? 'Copied ✅' : 'Copy Layer URL 🔗'}
-						</button>
+					<Form formConfig={formConfig} encodedParams={encodedParams}>
+						<FullScreenButtons
+							compositionName="Layer"
+							fullScreenUrl={layerUrl}
+						/>
 					</Form>
 				</Sidebar>
 			</ResizeWrapper>

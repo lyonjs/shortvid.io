dedf550fe67ebdd41a768e61bda1c3320e546541
emeric <emericboy73@gmail.com>
2023-08-07 11:38:24 +0200

    uncomment content

--- app/page.tsx
+++ app/page.tsx
@@ -63,51 +63,51 @@ export default function Home() {
 					</div>
 				</div>
 
-				{/* <h2 className="text-2xl pb-4 font-bold"> */}
-				{/* 	What is the goal of this projet ? */}
-				{/* </h2> */}
+				<h2 className="text-2xl pb-4 font-bold">
+					What is the goal of this projet ?
+				</h2>
 
-				{/* <p className="mt-2"> */}
-				{/* 	For the moment, templates are not very generic and are branded LyonJS */}
-				{/* 	but{' '} */}
-				{/* 	<strong> */}
-				{/* 		we have the ambition to provide much more generic templates that could */}
-				{/* 		be used by anyone who wants to generate videos for all the meetups, */}
-				{/* 		conferences or companies that want to use them. */}
-				{/* 	</strong> */}
-				{/* </p> */}
+				<p className="mt-2">
+					For the moment, templates are not very generic and are branded LyonJS
+					but{' '}
+					<strong>
+						we have the ambition to provide much more generic templates that
+						could be used by anyone who wants to generate videos for all the
+						meetups, conferences or companies that want to use them.
+					</strong>
+				</p>
 
-				{/* <p className="mt-2 font-bold"> */}
-				{/* 	If you are a meetup or conference organizer and you are interested in */}
-				{/* 	this tool and you would like to have some help,{' '} */}
-				{/* 	<a href="https://github.com/lyonjs/shortvid.io/discussions/categories/conference-meetup-communication"> */}
-				{/* 		don&lsquo;t hesitate to open a discussion on our Github and we will */}
-				{/* 		see what can be done. */}
-				{/* 	</a> */}
-				{/* </p> */}
+				<p className="mt-2 font-bold">
+					If you are a meetup or conference organizer and you are interested in
+					this tool and you would like to have some help,{' '}
+					<a href="https://github.com/lyonjs/shortvid.io/discussions/categories/conference-meetup-communication">
+						don&lsquo;t hesitate to open a discussion on our Github and we will
+						see what can be done.
+					</a>
+				</p>
 
-				{/* <p className="mt-2"> */}
-				{/* 	We know it&lsquo;s a pain to generate all the communication materials */}
-				{/* 	for an event, so this project allows us to generate everything from */}
-				{/* 	configuration JSON. */}
-				{/* </p> */}
+				<p className="mt-2">
+					We know it&lsquo;s a pain to generate all the communication materials
+					for an event, so this project allows us to generate everything from
+					configuration JSON.
+				</p>
 
-				{/* <p className="mt-2"> */}
-				{/* 	We can generate <strong>MP4 videos, Gif, JPEG, PNG</strong> in a */}
-				{/* 	programmatic way. Some common uses: */}
-				{/* </p> */}
+				<p className="mt-2">
+					We can generate <strong>MP4 videos, Gif, JPEG, PNG</strong> in a
+					programmatic way. Some common uses:
+				</p>
 
-				{/* <ul className="mt-2 list-disc pl-8"> */}
-				{/* 	<li>generate a video to announce your sponsors</li> */}
-				{/* 	<li> */}
-				{/* 		generate images for the display screens of your conference venues. */}
-				{/* 	</li> */}
-				{/* 	<li> */}
-				{/* 		generate videos for your speakers to announce their participation and */}
-				{/* 		talks. */}
-				{/* 	</li> */}
-				{/* 	<li>And many more ideas...</li> */}
-				{/* </ul> */}
+				<ul className="mt-2 list-disc pl-8">
+					<li>generate a video to announce your sponsors</li>
+					<li>
+						generate images for the display screens of your conference venues.
+					</li>
+					<li>
+						generate videos for your speakers to announce their participation
+						and talks.
+					</li>
+					<li>And many more ideas...</li>
+				</ul>
 
 				<h2 className="text-2xl pb-4 pt-8 font-bold">
 					Conference and partner events

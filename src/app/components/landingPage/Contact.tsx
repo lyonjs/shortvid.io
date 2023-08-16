194d4bec1ce41a2025b0615cff1059caed816a57
emeric <emericboy73@gmail.com>
2023-08-16 11:18:14 +0200

    created card with gradient border. WARN: experimental css

--- src/app/components/landingPage/Contact.tsx
+++ src/app/components/landingPage/Contact.tsx
@@ -1,6 +1,8 @@
 import {Icon} from '@iconify/react';
 import Image from 'next/image';
 
+import {Card} from './Card';
+
 import styles from '../../../../styles/app/layout/landing/contact.module.css';
 
 const contacts = [
@@ -25,16 +27,18 @@ export const Contact = () => {
 
 			<div className={styles.list}>
 				{contacts.map(({name, twitter, pictureUrl}) => (
-					<div key={name} className={styles.contact}>
-						<Image src={pictureUrl} alt={name} width={150} height={150} />
-						<div>
-							<h4>{name}</h4>
-							<a href={`https://twitter.com/${twitter}`}>
-								<Icon icon="mdi:twitter" />
-								{twitter}
-							</a>
+					<Card key={name}>
+						<div className={styles.contact}>
+							<Image src={pictureUrl} alt={name} width={150} height={150} />
+							<div>
+								<h4>{name}</h4>
+								<a href={`https://twitter.com/${twitter}`}>
+									<Icon icon="mdi:twitter" />
+									{twitter}
+								</a>
+							</div>
 						</div>
-					</div>
+					</Card>
 				))}
 			</div>
 		</section>

10c25c4150e8cccf5f71006c660db62c712bfbf5
emeric <emericboy73@gmail.com>
2023-08-17 16:39:30 +0200

    add config for landing page

--- src/app/components/landingPage/Sponsors.tsx
+++ src/app/components/landingPage/Sponsors.tsx
@@ -1,22 +1,11 @@
 import Image from 'next/image';
 
+import {sponsors} from '../../../data/config/landingPageConfig';
+
 import {Card} from './Card';
 import {ListBloc} from './ListBloc';
 
 import styles from '../../../../styles/app/layout/landing/parterAndSponsors.module.css';
-
-const sponsors = [
-	{
-		name: 'Vercel',
-		logo: '/images/app/sponsors/vercelLogo.svg',
-		url: 'https://vercel.com/home',
-	},
-	{
-		name: 'Clever Cloud',
-		logo: '/images/app/sponsors/cleverCloudLogo.svg',
-		url: 'https://www.clever-cloud.com/',
-	},
-];
 
 export const Sponsors = () => {
 	return (
---
+++ src/data/config/landingPageConfig.js
@@ -0,0 +1,12 @@
+export const sponsors = [
+	{
+		name: 'Vercel',
+		logo: '/images/app/sponsors/vercelLogo.svg',
+		url: 'https://vercel.com/home',
+	},
+	{
+		name: 'Clever Cloud',
+		logo: '/images/app/sponsors/cleverCloudLogo.svg',
+		url: 'https://www.clever-cloud.com/',
+	},
+];

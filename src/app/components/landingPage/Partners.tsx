12b5e4b00cad8ab2ab592e8cb9cd17277ca39684
emeric <emericboy73@gmail.com>
2023-08-17 16:46:29 +0200

    use config file

--- src/app/components/landingPage/Partners.tsx
+++ src/app/components/landingPage/Partners.tsx
@@ -1,32 +1,11 @@
 import Image from 'next/image';
 
+import {partners} from '../../../data/config/landingPageConfig';
+
 import {Card} from './Card';
 import {ListBloc} from './ListBloc';
 
 import styles from '../../../../styles/app/layout/landing/parterAndSponsors.module.css';
-
-const partners = [
-	{
-		name: 'Touraine Tech',
-		logo: '/images/showcases/touraineTech/touraineTechLogo.svg',
-		url: 'https://touraine.tech/',
-	},
-	{
-		name: 'Mixit',
-		logo: '/images/showcases/mixit/mixitLogo2023.svg',
-		url: 'https://mixitconf.org/',
-	},
-	{
-		name: 'Camping des Speakers',
-		logo: '/images/showcases/campingDesSpeakers/campingDesSpeakersLogo.png',
-		url: 'https://camping-speakers.fr/',
-	},
-	{
-		name: 'Volcamp',
-		logo: '/images/showcases/volcamp/volcampText.svg',
-		url: 'https://www.volcamp.io/',
-	},
-];
 
 export const Partners = () => {
 	return (
--- src/data/config/landingPageConfig.js
+++ src/data/config/landingPageConfig.js
@@ -12,3 +12,26 @@ export const SPONSORS = [
 		url: 'https://www.clever-cloud.com/',
 	},
 ];
+
+export const partners = [
+	{
+		name: 'Touraine Tech',
+		logo: '/images/showcases/touraineTech/touraineTechLogo.svg',
+		url: 'https://touraine.tech/',
+	},
+	{
+		name: 'Mixit',
+		logo: '/images/showcases/mixit/mixitLogo2023.svg',
+		url: 'https://mixitconf.org/',
+	},
+	{
+		name: 'Camping des Speakers',
+		logo: '/images/showcases/campingDesSpeakers/campingDesSpeakersLogo.png',
+		url: 'https://camping-speakers.fr/',
+	},
+	{
+		name: 'Volcamp',
+		logo: '/images/showcases/volcamp/volcampText.svg',
+		url: 'https://www.volcamp.io/',
+	},
+];

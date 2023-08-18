138611040b87dd7ab37b275ccb622bb19b6ae2c6
emeric <emericboy73@gmail.com>
2023-08-18 10:02:15 +0200

    review changes: updated link

--- src/app/components/landingPage/Contact.tsx
+++ src/app/components/landingPage/Contact.tsx
@@ -1,5 +1,7 @@
 import {Icon} from '@iconify/react';
 import Image from 'next/image';
+import Link from 'next/link';
+import {staticFile} from 'remotion';
 
 import {Card} from './Card';
 
@@ -9,14 +11,12 @@ const contacts = [
 	{
 		name: 'Antoine Carron',
 		twitter: 'Slashgear_',
-		pictureUrl:
-			'https://pbs.twimg.com/profile_images/1633242263860965376/iw95gEVA_400x400.jpg',
+		pictureUrl: staticFile('/images/app/contact/antoine-carron.jpeg'),
 	},
 	{
 		name: 'Mickael Alves',
 		twitter: 'CruuzAzul',
-		pictureUrl:
-			'https://pbs.twimg.com/profile_images/1635742650090283013/9qflRryB_400x400.jpg',
+		pictureUrl: staticFile('/images/app/contact/mickael-alves.jpeg'),
 	},
 ];
 
@@ -27,18 +27,20 @@ export const Contact = () => {
 
 			<div className={styles.list}>
 				{contacts.map(({name, twitter, pictureUrl}) => (
-					<Card key={name}>
-						<div className={styles.contact}>
-							<Image src={pictureUrl} alt={name} width={150} height={150} />
-							<div>
-								<h4>{name}</h4>
-								<a href={`https://twitter.com/${twitter}`}>
-									<Icon icon="mdi:twitter" />
-									{twitter}
-								</a>
+					<Link key={name} href={`https://twitter.com/${twitter}`}>
+						<Card>
+							<div className={styles.contact}>
+								<Image src={pictureUrl} alt={name} width={150} height={150} />
+								<div>
+									<h4>{name}</h4>
+									<span className={styles.twitter}>
+										<Icon icon="mdi:twitter" />
+										{twitter}
+									</span>
+								</div>
 							</div>
-						</div>
-					</Card>
+						</Card>
+					</Link>
 				))}
 			</div>
 		</section>

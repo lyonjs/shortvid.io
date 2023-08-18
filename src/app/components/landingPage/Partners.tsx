f96ccca6714345ae55b59eb7702189c64ae16ae8
emeric <emericboy73@gmail.com>
2023-08-18 11:20:56 +0200

    add dark theme partners

--- src/app/components/landingPage/Partners.tsx
+++ src/app/components/landingPage/Partners.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import {partners} from '../../../data/config/landingPageConfig';
+import {useSelectedTheme} from '../../hooks/useSelectedTheme';
 
 import {Card} from './Card';
 import {ListBloc} from './ListBloc';
@@ -9,13 +10,21 @@ import {ListBloc} from './ListBloc';
 import styles from '../../../../styles/app/layout/landing/parterAndSponsors.module.css';
 
 export const Partners = () => {
+	const {themeName} = useSelectedTheme();
+
 	return (
 		<ListBloc sectionTitle="They worked with us">
-			{partners.map(({name, logo, url}) => (
+			{partners.map(({name, logo, logoDark, url}) => (
 				<Link key={name} href={url} target="_blank">
 					<Card>
 						<div className={styles.logos}>
-							<Image width={300} height={300} src={logo} alt={name} />
+							<Image
+								className={styles.sponsorLogos}
+								width={300}
+								height={300}
+								src={themeName == 'dark' ? logoDark : logo}
+								alt={name}
+							/>
 						</div>
 					</Card>
 				</Link>
--- src/data/config/landingPageConfig.js
+++ src/data/config/landingPageConfig.js
@@ -16,22 +16,26 @@ export const SPONSORS = [
 export const partners = [
 	{
 		name: 'Touraine Tech',
-		logo: '/images/showcases/touraineTech/touraineTechLogo.svg',
+		logo: '/images/app/partners/light/touraineTechLogo.svg',
+		logoDark: '/images/app/partners/dark/touraineTechLogo.svg',
 		url: 'https://touraine.tech/',
 	},
 	{
 		name: 'Mixit',
-		logo: '/images/showcases/mixit/mixitLogo2023.svg',
+		logo: '/images/app/partners/light/mixitLogo.svg',
+		logoDark: '/images/app/partners/dark/mixitLogo.svg',
 		url: 'https://mixitconf.org/',
 	},
 	{
 		name: 'Camping des Speakers',
 		logo: '/images/showcases/campingDesSpeakers/campingDesSpeakersLogo.png',
+		logoDark: '/images/showcases/campingDesSpeakers/campingDesSpeakersLogo.png',
 		url: 'https://camping-speakers.fr/',
 	},
 	{
 		name: 'Volcamp',
 		logo: '/images/showcases/volcamp/volcampText.svg',
+		logoDark: '/images/showcases/volcamp/volcampText.svg',
 		url: 'https://www.volcamp.io/',
 	},
 ];

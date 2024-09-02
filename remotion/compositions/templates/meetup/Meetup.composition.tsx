45a7a54cd186599bf883b285e953edbd5eb1f366
Antoine Caron <acaron@scaleway.com>
2024-09-02 21:28:58 +0200

    refactor(compositions): enhance Meetup and Talk compositions

--- remotion/compositions/templates/meetup/Meetup.composition.tsx
+++ remotion/compositions/templates/meetup/Meetup.composition.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Composition, Folder} from 'remotion';
 
-import {Meetup} from './Meetup';
+import {Meetup, MeetupSchema} from './Meetup';
 import {Register} from './Register';
 
 export const MeetupComposition: React.FC = () => {
@@ -14,9 +14,14 @@ export const MeetupComposition: React.FC = () => {
 				id="Meetup"
 				fps={30}
 				durationInFrames={270}
+				schema={MeetupSchema}
 				defaultProps={{
 					title: '#76 - LyonJS de la rentrée 🦁',
 					date: '28 septembre 2022',
+					backgroundImg:
+						'https://github.com/lyonjs/shortvid.io/blob/48ffea960b300eb9230786ed0ab043ec3387e877/public/images/showcases/lyonjs/defaultBackgroundImage.jpeg?raw=true',
+					eventLogo:
+						'https://github.com/lyonjs/shortvid.io/blob/main/public/images/showcases/lyonjs/lyonjsSquaredLogo.png?raw=true',
 				}}
 			/>
 			<Composition

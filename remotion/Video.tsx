2273605705c2c37e4d35814b2fba8886f082eff2
malves.externe <malves.externe@bedrockstreaming.com>
2026-02-19 22:59:23 +0100

    refactor: remove design components

--- remotion/Video.tsx
+++ remotion/Video.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {Folder} from 'remotion';
 
 import {ShowcasesComposition} from './compositions/showcases/Showcases.composition';
 import {LyonJSMeetup} from './compositions/templates/LyonJSMeetup.composition';
 import {MeetupToolKit} from './compositions/templates/MeetupToolKit.composition';
-import {AtomsComposition} from './design/atoms/Atoms.composition';
-import {MoleculesComposition} from './design/molecules/Molecules.composition';
 
 export const RemotionRoot: React.FC = () => {
 	return (
@@ -13,10 +10,6 @@ export const RemotionRoot: React.FC = () => {
 			<LyonJSMeetup />
 			<MeetupToolKit />
 			<ShowcasesComposition />
-			<Folder name="Design">
-				<AtomsComposition />
-				<MoleculesComposition />
-			</Folder>
 		</>
 	);
 };

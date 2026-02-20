83bd7ff705b936b75d8862813e831abe8eb79fc9
malves.externe <malves.externe@bedrockstreaming.com>
2026-02-20 20:30:05 +0100

    refactor: remove MeetupToolkit components

--- remotion/Video.tsx
+++ remotion/Video.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
 import {LyonJSMeetup} from './compositions/templates/LyonJSMeetup.composition';
-import {MeetupToolKit} from './compositions/templates/MeetupToolKit.composition';
 
 export const RemotionRoot: React.FC = () => {
 	return (
-		<>
 			<LyonJSMeetup />
-			<MeetupToolKit />
-		</>
 	);
 };
